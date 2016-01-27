af2b6ce4e237fa02c4329e21528e74be7f0ff779
Chris Clark <chrisclark1979@gmail.com>
2016-01-27 13:31:40 -0800

    challenge view renders game messages, initial code

--- src/client/app/challenge/challengeCtrl.js
+++ src/client/app/challenge/challengeCtrl.js
@@ -4,14 +4,18 @@ angular.module('duel.challengeCtrl', ['duel.challengeFact'])
   $scope.currentUser = $stateParams.userid;
   $scope.gameid = $stateParams.gameid;
   $scope.data = {}
-  $scope.question = ChallengeFact.client.question;
+
+  $scope.getMessage = ChallengeFact.getMessage;
   $scope.getQuestion = ChallengeFact.getQuestion;
-  $scope.winner = ChallengeFact.endGame();
-  if(!$scope.winner === false){
-    alert($scope.winner() + ' won the challenge!')
-    $state.go('lobby', {
-      userid: $scope.userid
-    });
+  $scope.getInitial = ChallengeFact.getInitial;
+  $scope.getWinner = ChallengeFact.getWinner;
+  
+  if($scope.getWinner() !== null){
+    console.log("if statement working");
+    // alert($scope.winner + ' won the challenge!')
+    // $state.go('lobby', {
+    //   userid: $scope.userid
+    // });
   }
   //Calls ChallengeFact's connectToGame() once the user enters the 'challenge' state
   ChallengeFact.connectToGame({
