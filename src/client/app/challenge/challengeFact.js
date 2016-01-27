31a57c8af4b3b8ff7a3fb456c494beae16c63d57
Chris Clark <chrisclark1979@gmail.com>
2016-01-27 13:31:40 -0800

    challenge view renders game messages, initial code

--- src/client/app/challenge/challengeFact.js
+++ src/client/app/challenge/challengeFact.js
@@ -3,13 +3,22 @@ angular.module('duel.challengeFact', ['duel.socketFact'])
 .factory('ChallengeFact', ['SocketFact', '$rootScope', function(SocketFact, $rootScope) {
   var challengeFact = {};
   challengeFact.client = {
-    question: "question loading...",
-    winner: false
+    question: "question will be displayed when challenge commences",
+    message: "Waiting for opponent...",
+    initial: "Your Code Here",
+    winner: null
   };
   challengeFact.getQuestion = function(){
     return challengeFact.client.question;
   }
-  challengeFact.endGame = function(){
+  challengeFact.getMessage = function(){
+    return challengeFact.client.message;
+  }
+   challengeFact.getInitial = function(){
+    return challengeFact.client.initial;
+  }
+  challengeFact.getWinner = function(){
+    console.log("getting winner: " + challengeFact.client.winner)
     return challengeFact.client.winner;
   }
   //****************
@@ -18,20 +27,25 @@ angular.module('duel.challengeFact', ['duel.socketFact'])
 
   SocketFact.socket.on('challenge/gameStart', function(data) {
     console.log("Game has begun", data);
+    challengeFact.client.message = "The challenge has begun";
     challengeFact.client.question = data.question;
+    challengeFact.client.initial = data.initialCode;
     //should refactor to not use rootScope?
     $rootScope.$apply();
   });
 
   SocketFact.socket.on('challenge/invalidSolution', function(data) {
-    console.log('Invalid solution!');
-    console.log(data);
+    console.log('Invalid solution!', data);
+    challengeFact.client.message = "Your solution was invalid.  Please try again.";
+    $rootScope.$apply();
   })
 
   SocketFact.socket.on('challenge/winner', function(data) {
     console.log(data.winner + ' won the challenge!');
     challengeFact.client.winner = data.winner;
+    challengeFact.client.message = data.winner + ' won the challenge!  Taking you back to the lobby...';
     //should refactor to not use rootScope?
+    challengeFact.getWinner();
     $rootScope.$apply();
   })
 
