a83d6a195cda5f3e00abedcba8799cd864c07a11
Chris Clark <chrisclark1979@gmail.com>
2016-01-27 21:42:22 -0800

    Initial code rendering correctly; players are rerouted to Lobby post-game

--- src/client/app/challenge/challengeCtrl.js
+++ src/client/app/challenge/challengeCtrl.js
@@ -1,22 +1,18 @@
 angular.module('duel.challengeCtrl', ['duel.challengeFact'])
 
 .controller('ChallengeCtrl', ['$scope', '$state', '$stateParams', 'ChallengeFact', function($scope, $state, $stateParams, ChallengeFact) {
+  angular.extend($scope, ChallengeFact);
+  
   $scope.currentUser = $stateParams.userid;
   $scope.gameid = $stateParams.gameid;
-  $scope.data = {}
-
-  $scope.getMessage = ChallengeFact.getMessage;
-  $scope.getQuestion = ChallengeFact.getQuestion;
-  $scope.getInitial = ChallengeFact.getInitial;
-  $scope.getWinner = ChallengeFact.getWinner;
+  $scope.data = {};
+  $scope.data.solution = $scope.client.initial;
   
-  if($scope.getWinner() !== null){
-    console.log("if statement working");
-    // alert($scope.winner + ' won the challenge!')
-    // $state.go('lobby', {
-    //   userid: $scope.userid
-    // });
-  }
+  //updates solution textarea when initial code is loaded
+  $scope.$watch('client.initial', function(newVal, oldVal){
+    $scope.data.solution = $scope.client.initial;
+  }, true);
+
   //Calls ChallengeFact's connectToGame() once the user enters the 'challenge' state
   ChallengeFact.connectToGame({
     userid: window.localStorage.getItem('duel.userid'),
