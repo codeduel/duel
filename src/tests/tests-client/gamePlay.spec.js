b5c4b283e61968b66835552cb1f03bcc3a383480
Chris Clark <chrisclark1979@gmail.com>
2016-02-04 00:19:00 -0800

    file clean up

--- src/tests/tests-client/gamePlay.spec.js
+++
@@ -1,9 +0,0 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var app = require('../../client/app.js');
-var should = chai.should();
-
-chai.use(chaiHttp);
-describe('Blobs', function() {
-  it('should list ALL blobs on /blobs GET')
-});
