8d7571764753d1da3f3b662e7f72972197cffe5c
Becky Jaimes <beckyjaimes@gmail.com>
2016-02-17 17:13:50 -0800

    added tracking to themes

--- src/client/app/wrap/navBar/navBarCtrl.js
+++ src/client/app/wrap/navBar/navBarCtrl.js
@@ -3,6 +3,10 @@ angular.module('duel.wrap.navBarCtrl', [])
 .controller('NavBarCtrl', ['$scope', 'ThemeFact', 'UserFact', function($scope, ThemeFact, UserFact) {
   $scope.changeTheme = function(themeName){
       ThemeFact.setTheme(themeName);
+
+      analytics.track('Changed Theme', {
+        themeName: themeName
+      });
   };
 
   $scope.login = function() {
