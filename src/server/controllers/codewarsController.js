35797c3920edc23a07121b402d32fb1439f8daec
Becky Jaimes <beckyjaimes@gmail.com>
2016-02-21 10:04:54 -0800

    added more challenges making usre all of them are js, ruby and python

--- src/server/controllers/codewarsController.js
+++ src/server/controllers/codewarsController.js
@@ -26,51 +26,64 @@ var config = {
   //Dictionary object that maps front-end descriptions to API-specific strings as key-value pairs (respectively)
   difficultyMappings: {
     easy: [
-      'count-by-x',
-      'stringy-strings',
-      'convert-number-to-reversed-array-of-digits',
-      'beginner-series-number-3-sum-of-numbers',
-      'exes-and-ohs',
-      'credit-card-mask',
-      'the-coupon-code',
-      'tricky-doubles',
-      'sexy-primes-<3',
-      'password-validator',
-      'array-of-primes',
-      'all-none-and-any',
-      'binary-addition',
-      'count-by-x'
+      'count-by-x',                   //js, ruby, python, haskell
+      'stringy-strings',              //js, ruby, python, haskell, cofeescript, C#, java
+      'convert-number-to-reversed-array-of-digits',  //js, ruby, python, haskell, coffeescript, clojure
+      'beginner-series-number-3-sum-of-numbers',     //js, ruby, python, cofeescript, C#, java
+      'exes-and-ohs',                 //js, ruby, python, haskell, cofeescript
+      'credit-card-mask',             //js, ruby, python, haskell, cofeescript, C#
+      'binary-addition',              //js, ruby, python
+      'broken-greetings',             //js, ruby, python, haskell, cofeescript, C#, clojure, java
+      'divisible-by-four',            //js, ruby, python, C#
+      'unexpected-parsing',           //js, ruby, python
+      'stringy-strings',              //js, ruby, python, haskell, cofeescript, C#, java
+      'sum-arrays',                   //js, ruby, python, haskell, cofeescript, clojure
+      'convert-a-string-to-a-number', //js, ruby, python, haskell, cofeescript, C#, java, clojure
+      'count-by-x'                    //js, ruby, python, haskell
+      // 'the-coupon-code',             //js, C#
+      // 'tricky-doubles',              //js, ruby
+      // 'sexy-primes-<3',              //js, python, C#
+      // 'password-validator',          //js
+      // 'array-of-primes',             //js -> this problem is too dificult for beginers 
+      // 'all-none-and-any',            //js, haskell
 
     ],
     medium: [
-      'jaden-casing-strings',
-      'list-to-array',
-      'remove-the-minimum',
-      'unique-in-order',
-      'longest-palindrome',
-      'sum-of-pairs',
-      'moving-zeros-to-the-end',
-      'weird-string-case',
-      'your-order-please',
-      'find-the-divisors',
-      'sum-consecutives'
+      'jaden-casing-strings',           //js, ruby, python, haskell, coffeescript, java, clojure
+      'is-n-divisible-by-dot-dot-dot',  //js, ruby, python, haskell
+      'remove-the-minimum',             //js, ruby, python, haskell, cofeescript, C#
+      'unique-in-order',                //js, ruby, python
+      'longest-palindrome',             //js, ruby, python
+      'sum-of-pairs',                   //js, ruby, python
+      'weird-string-case',              //js, ruby, python, haskell, coffeescript
+      'find-the-divisors',              //js, ruby, python, haskell, coffeescript
+      'x-marks-the-spot',               //js, ruby, python
+      'sum-consecutives'                //js, ruby, python, haskell, coffeescript, clojure, java
+      // 'your-order-please',              //js, python, java
+      // 'list-to-array',                  //js, python
+      // 'moving-zeros-to-the-end',        //js, cofeescript
     ],
     hard: [
-      'snail',
-      'sum-strings-as-numbers',
-      'next-bigger-number-with-the-same-digits',
-      'valid-braces',
-      'pascals-triangle',
-      'calculating-with-functions',
-      'a-chain-adding-function',
-      'guess-the-gifts',
-      'valid-parentheses',
-      'moving-zeros-to-the-end',
-      'merged-string-checker'
+      'snail',                         //js, ruby, python, haskell, coffeescript
+      'next-bigger-number-with-the-same-digits', //js, ruby, python, C#
+      'convert-string-to-camel-case',  //js, python, ruby, haskell, coffeescript,  C#
+      'valid-braces',                  //js, python, haskell, coffeescript, java
+      'valid-parentheses',             //js, ruby, python, haskell, coffeescript
+      'first-n-prime-numbers',         //js, ruby, python
+      'last-digit-of-a-large-number',  //js, ruby, python, haskell
+      'feynmans-square-question',      //js, ruby, python, haskell
+      'did-i-finish-my-sudoku'         //js, ruby, python
+      // 'sum-strings-as-numbers',        //js, C#
+      // 'pascals-triangle',              //js, ruby, coffeescript, C#, haskell
+      // 'calculating-with-functions',    //js, ruby
+      // 'a-chain-adding-function',       //js
+      // 'guess-the-gifts',               //js, ruby, coffeescript
+      // 'moving-zeros-to-the-end',       //js, coffeescript
+      // 'merged-string-checker'          //js, python, haskell, coffeescript, clojure, java, C#
 
     ],
     demo: [
-      'multiply'
+      'multiply' //js, ruby, python, haskell, coffeescript, clojure, java, C#
     ]
   }
 };
